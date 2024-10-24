fb3e13d63cd8e7bdad7b8a8cfb4fa5d24a0fc228
USER <USER@DESKTOP-8N6EMQG>
2024-10-24 11:34:18 +0530

    first commit

---
+++ MVN/src/test/java/com/actitime/testscript/CreateCustemer.java
@@ -0,0 +1,15 @@
+package com.actitime.testscript;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateCustemer {
+	@Test
+	public void createCustemer() {
+
+		Reporter.log("createCustemer", true);
+
+	}
+
+}
